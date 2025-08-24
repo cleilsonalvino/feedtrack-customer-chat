3f0decea73f37a80fe0ff6d0b214482f9f770608
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-08-24 15:07:45 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3006/api/v1", // ou sua URL de produção
+  baseURL: "https://server.feedtrack.site/api/v1", // ou sua URL de produção
 });
 
 // Sempre que recarregar a página, adiciona o token do localStorage
